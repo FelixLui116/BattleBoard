c99d8a830e9d76061fb22b941d7a1bcd83f370cc
FelixLui116 <37116340+FelixLui116@users.noreply.github.com>
2023-06-07 16:17:19 +0100

    update  ParrelSync, sycn create object

--- Assets/Script/BoardNode.cs
+++ Assets/Script/BoardNode.cs
@@ -13,7 +13,8 @@ public class BoardNode : NetworkBehaviour // NetworkBehaviour MonoBehaviour
     [SerializeField] private GameObject Node;
     [SerializeField] private Vector2 NodePosition;
 
-    private GameObject Monster_Ghost;
+    // private GameObject Monster_Ghost;
+    private NetworkObject Monster_Ghost;
 
     private bool mouseClick_bool = false;
 
@@ -60,48 +61,52 @@ public class BoardNode : NetworkBehaviour // NetworkBehaviour MonoBehaviour
         }
         
         // Node_renderer.material.color = Color.yellow;
-        if (IsHost){
-            ColorServerToClient_ClientRpc(Color.yellow);
-        }else{
-            ChangeColorFunc_Green_ServerRpc(Color.yellow);
-        }
+        // if (IsHost){
+        //     ChangeColor_func_ClientRpc(Color.yellow);
+        // }else{
+        //     ChangeColor_func_ServerRpc(Color.yellow);
+        // }
+        ChangeColor_func(Color.yellow);
             
         
         if (Monster_Ghost != null){
             
-            if (Monster_Ghost.name != Monster_.name){
-                Destroy_Ghost();
+            if (Monster_Ghost.gameObject.name != Monster_.name){
+                Destroy_Ghost(Monster_Ghost.gameObject);
                 CloneMonsterGhost_func(Monster_); 
             }
 
-            Monster_Ghost.SetActive(true);
+            // Monster_Ghost.gameObject.SetActive(true);
             
             return;
         }
 
-        ////// 
-        // if (Monster_Ghost.name != Monster_.name)
-        // {
-        //     Destroy_Ghost();
-        // }
         CloneMonsterGhost_func(Monster_); 
     }
 
-    [ServerRpc]
-    private void ChangeColorFunc_Green_ServerRpc(Color _c){
-        Debug.Log("Launch on Server");
-        Node_renderer.material.color = _c;
 
-        // if(!IsHost){
-        //     ColorServerToClient_ClientRpc(_c);
-        // }
+    // change Node Color
+    private void ChangeColor_func(Color c){
+        if (IsServer)
+        {
+            ChangeColor_func_ClientRpc(c);
+        }
+        else if (IsClient)
+        {
+            ChangeColor_func_ServerRpc(c);
+        };
     }
 
+    [ServerRpc(RequireOwnership = false)] // Allow client edit to server
+    private void ChangeColor_func_ServerRpc(Color _c){
+        Debug.Log("Launch on Server");
+        Node_renderer.material.color = _c;
+    }
     [ClientRpc]
-    private void ColorServerToClient_ClientRpc(Color _c)
+    private void ChangeColor_func_ClientRpc(Color _c)
     {
         Debug.Log("Launch on ClientRpc");
-        Node_renderer.material.color = Color.yellow;
+        Node_renderer.material.color = _c;
     }
 
 
@@ -117,10 +122,14 @@ public class BoardNode : NetworkBehaviour // NetworkBehaviour MonoBehaviour
         {
             return;
         }
+        if(Playerinfo.Instance.Get_player_SelectCard() ==null){
+            return;
+        }
 
         mouseClick_bool = true;
-        
-        Node_renderer.material.color = Color.green;
+
+        // Node_renderer.material.color = Color.green;
+        ChangeColor_func(Color.green);
         Debug.Log("Is mouse click");
         Playerinfo.Instance.Clean_SeletcCard();
         CloneMonster(Monster_);
@@ -135,64 +144,147 @@ public class BoardNode : NetworkBehaviour // NetworkBehaviour MonoBehaviour
         if (Monster_Ghost == null){
             return;
         }else{
-            Monster_Ghost.SetActive(false);
+            Monster_Ghost.gameObject.SetActive(false);
         }
 
         if (mouseClick_bool){
             return;
         }
 
-        Node_renderer.material.color = Color.white;
+        // Node_renderer.material.color = Color.white;
+        ChangeColor_func(Color.white);
         
     }
 
-    
+
+     // Add Clone
+    private void AddClone_func(GameObject go){
+        NetworkObject n_obj = go.GetComponent<NetworkObject>();
+        ulong networkObjectId = n_obj.NetworkObjectId;
+        if (IsServer)
+        {
+            AddClone_func_ClientRpc(networkObjectId);
+        }
+        else if (IsClient)
+        {
+            AddClone_func_ServerRpc(networkObjectId);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)] // Allow client edit to server
+    private void AddClone_func_ServerRpc(ulong networkObjectId)
+    {
+        Debug.Log("Launch on Server");
+
+        AddClone_func_ClientRpc(networkObjectId);
+    }
+    [ClientRpc]
+    private void AddClone_func_ClientRpc(ulong networkObjectId)
+    {
+        Debug.Log("Launch on ClientRpc");
+        NetworkObject no = null;
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(networkObjectId, out NetworkObject clientNetworkObject))
+        {
+            no = clientNetworkObject;
+        }
+        GameObject go = no.gameObject;
+        CloneMonsterGhost_func(go);
+    }
+
     private void CloneMonsterGhost_func(GameObject prefab){
-        GameObject go;
-        go = GameObject.Instantiate(prefab, this.gameObject.transform.position, Quaternion.identity , this.gameObject.transform);
+        // GameObject go;
+        // go = GameObject.Instantiate(prefab, this.gameObject.transform.position, Quaternion.identity , this.gameObject.transform);
         
-        go.GetComponent<NetworkObject>().Spawn(true);
-        // go.transform.position = new Vector3(0 ,0 ,0);
-        go.name += "_Ghost";
-        go.transform.localScale = new Vector3(10 , 10 , 10);
-        Monster_Ghost = go;
+        // go.GetComponent<NetworkObject>().Spawn(true);
+        // // go.transform.position = new Vector3(0 ,0 ,0);
+        // go.name += "_Ghost";
+        // go.transform.localScale = new Vector3(10 , 10 , 10);
+        // Monster_Ghost = go;
+
+        GameObject spawnedTestObject_clone = GameObject.Instantiate(prefab, this.gameObject.transform.position, Quaternion.identity , this.gameObject.transform);
+
+        NetworkObject networkObject = spawnedTestObject_clone.GetComponent<NetworkObject>();
+        networkObject.transform.localScale = new Vector3(10, 10, 10);
+        networkObject.name += "_Ghost";
+        networkObject.Spawn(true);
+        networkObject.gameObject.SetActive(true);
+        
+        // networkObject.SpawnAsPlayerObject(NetworkManager.Singleton.LocalClientId);
+        ulong networkObjectId = networkObject.NetworkObjectId;
+        Debug.Log("Network Object ID: " + networkObjectId);
+        // Add the object to the server's ownership
+        if (IsClient)
+        {
+        // NetworkManager.Singleton.SpawnManager.SpawnedObjects.Add(networkObjectId, networkObject);
+            networkObject.ChangeOwnership(NetworkManager.Singleton.LocalClientId);
+        }
+
+
+        AddObjectToClient(networkObjectId ); //, Monster_Ghost
 
         // NEED ADD model 
     }
 
-    private void Destroy_Ghost(){
-        //string ghostName = Monster_Ghost.name;
-        // GameObject Object = this.gameObject;
-        // for (int i = 0; i < Object.transform.childCount; i++)
-        // {
-        //     Debug.Log( Object.transform.GetChild(i).gameObject.name );
-        //     if (Object.transform.GetChild(i).gameObject.name == Monster_Ghost.name + "(Clone)_Ghost" )
-        //     {
-                
-        //         Destroy(this.gameObject.transform.GetChild(i).gameObject);
-        //     }
-        //     // string M_name = Monster_Ghost.name;
-        // }
+////////////////////////////////////////////////////////////////////////////
+/////////////  add object to client
+
+private void AddObjectToClient(ulong networkObjectId){
+        if (IsServer)
+        {
+            AddObjectClientRpc(networkObjectId);
+        }
+        else if (IsClient)
+        {
+            AddObjectServerRpc(networkObjectId);
+        }
+    }
+    [ServerRpc(RequireOwnership = false)] // Allow client to add object to server
+    public void AddObjectServerRpc(ulong  tarage_obj)
+    {
+        AddObjectClientRpc(tarage_obj);
+    }
+    [ClientRpc]
+    public void AddObjectClientRpc(ulong networkObjectId)
+    {
+        // 在客户端中根据网络标识符查找相应的网络对象
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(networkObjectId, out NetworkObject clientNetworkObject))
+        {
+            Monster_Ghost = clientNetworkObject;
+        }
+    }
 
 
-        // GameObject go = this.gameObject.transform.GetChild(0).gameObject;
-        // if(go.name.contains("_Ghost"))  tag?
-
-        // if (this.gameObject.transform.childCount == 1)
-        // {
-        //     return;
-        // }
-        // Debug.Log(this.gameObject.transform.childCount);
-        Destroy(this.gameObject.transform.GetChild(1).gameObject);
+    private void Destroy_Ghost(GameObject go){
+        //////////////////////////////////////////////////
+        // Destroy(this.gameObject.transform.GetChild(1).gameObject);
+        // 找到要解除生成的 Monster Ghost 的 NetworkObject
+        if (go == null){
+            return;
+        }
+        NetworkObject networkObject = go.GetComponent<NetworkObject>();
+        if (networkObject != null)
+        {
+            // 解除生成 NetworkObject
+            networkObject.Despawn();
+        }
+        
     }
 
     public void CloneMonster(GameObject go){
-        GameObject Monsterclone = Instantiate(go, transform.position, transform.rotation , this.gameObject.transform);
+        // GameObject Monsterclone = Instantiate(go, transform.position, transform.rotation , this.gameObject.transform);
         
-        Monsterclone.GetComponent<NetworkObject>().Spawn(true);
-        Monsterclone.transform.localScale = new Vector3(10 , 10 , 10);
+        // Monsterclone.GetComponent<NetworkObject>().Spawn(true);
+        // Monsterclone.transform.localScale = new Vector3(10 , 10 , 10);
+        GameObject spawnedTestObject_clone = GameObject.Instantiate(go, this.gameObject.transform.position, Quaternion.identity , this.gameObject.transform);
 
-        Destroy_Ghost();
+        NetworkObject networkObject = spawnedTestObject_clone.GetComponent<NetworkObject>();
+        networkObject.transform.localScale = new Vector3(10, 10, 10);
+        networkObject.name += "_Ghost";
+        networkObject.Spawn(true);
+        networkObject.gameObject.SetActive(true);
+
+
+        Destroy_Ghost(Monster_Ghost.gameObject);
     }
 
     private void OnSomeValueChanged(int previous, int current)
